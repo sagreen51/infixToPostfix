c363854aa83426348962c72f7974f9ac4d6f5828
Steven <s.a.green51@gmail.com>
2017-11-25 23:32:32 -0800

    ...

--- prog3_2.lua
+++ prog3_2.lua
@@ -5,9 +5,9 @@ local function push(value)table.insert(temp,value)end
 
 function InfixToPostfix(str)
 --	print("Assignment 3-2, Steven Green, s.a.green51@gmail.com")
-	list[0] = ' '
-	temp[0] = ' '
-	for i, string in string.gmatch(str, "[^%s+]")do 
+--	list[0] = ' '
+--	temp[0] = ' '
+	for string in string.gmatch(str, "[^%s+]")do 
 	if string == '+' or string == '-' then 
 		push(string)
 	else
