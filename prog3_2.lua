2dd70b4fefd07bc4bad1664ee33d0e21e5712d50
Steven <s.a.green51@gmail.com>
2017-11-25 19:39:31 -0800

    test3

--- prog3_2.lua
+++ prog3_2.lua
@@ -1,4 +1,13 @@
-void main(){
+local list  = {}
+temp = {}
+newList = {}
 
+local function push(value) table.insert(list,value) end
+local function remove(index) table.remove(index)end 
 
-}
+local infixToPostfix(str)  
+for temp in string.gmatch(str,"[^%s]")do 
+newList[i] = temp
+end
+return newList
+end 
