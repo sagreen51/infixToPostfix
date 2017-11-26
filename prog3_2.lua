21c46c2174f3af85619857a1dcd2f8c68cb0fe9a
Steven <s.a.green51@gmail.com>
2017-11-25 21:44:17 -0800

    test5

--- prog3_2.lua
+++ prog3_2.lua
@@ -1,16 +1,21 @@
 local list  = {}
-temp = {}
 newList = {}
 
-local function push(value) table.insert(list,value) end
-local function remove(index) table.remove(index)end 
-
-local InfixToPostfix(str)
-i = 0  
-for temp in string.gmatch(str,"[^%s]")do 
-newList[i] = temp
-push(list,temp)
-i = i+1
+local function push(value)
+	table.insert(list,value)
+	strcat(list, " ")
 end
-return list, i
+
+function InfixToPostfix(str)
+	newList[0] = ' '
+	for temp in string.gmatch(str,"[^%s]")do 
+
+	if temp == '+' or temp == '-' then
+		push(temp)  
+	else strcat(newList,temp) strcat(newList, " ") 
+	end
+	end
+	total = 19
+	strcat(newList, list)
+	return newList, total
 end 
