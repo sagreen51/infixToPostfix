4a1b3b926909da0138b1299ccfd092005a00e995
Steven <s.a.green51@gmail.com>
2017-11-25 19:08:46 -0800

    test1

--- prog3_1.cpp
+++ prog3_1.cpp
@@ -4,6 +4,8 @@
 
 int  main(int argc,char *argv[]){
 
+	printf("Assignment #3-1, Steven Green, s.a.green51@gmail.com");
+
 	lua_State *state = luaL_newstate();
 	luaL_openlibs(state);
 	luaL_dofile(state,argv[1]);
