24c3b4458238898b20932b56c06755df1219e447
Steven <s.a.green51@gmail.com>
2017-11-25 22:27:30 -0800

    test5

--- prog3_2.lua
+++ prog3_2.lua
@@ -9,7 +9,7 @@ end
 local function pop(index) table.remove(str,index)end
 
 function InfixToPostfix(str)
-	print("Assignment 3-2, Steven Green, s.a.green51@gmail.com")
+--	print("Assignment 3-2, Steven Green, s.a.green51@gmail.com")
 	list[0] = " "
 	for i = 0, string.len(str)-1, 1 do 
 	if str[i] == "+" or str[i] =='-'then 
@@ -18,5 +18,5 @@ function InfixToPostfix(str)
 	end
 	end
 	table.concat(str,list)
-	return str,19
+	return str
 end
