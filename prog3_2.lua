7bebec7eba18bd35f84ebbf9a775b9b93c8804cf
Steven <s.a.green51@gmail.com>
2017-11-25 23:30:58 -0800

    ...

--- prog3_2.lua
+++ prog3_2.lua
@@ -7,14 +7,13 @@ function InfixToPostfix(str)
 --	print("Assignment 3-2, Steven Green, s.a.green51@gmail.com")
 	list[0] = ' '
 	temp[0] = ' '
-	i = 1
-	for string in string.gmatch(str, "[^%s]")do 
+	for i, string in string.gmatch(str, "[^%s+]")do 
 	if string == '+' or string == '-' then 
 		push(string)
 	else
 		list[i] = string
 		i = i+1
-	
+	end
 	end
 	return table.concat(list," ")
 end
