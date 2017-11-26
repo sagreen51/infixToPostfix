9a758a72fb7c00cbde0f16578f7f8847b5f47c69
Steven <s.a.green51@gmail.com>
2017-11-25 19:43:32 -0800

    test3

--- prog3_2.lua
+++ prog3_2.lua
@@ -8,6 +8,7 @@ local function remove(index) table.remove(index)end
 local infixToPostfix(str)  
 for temp in string.gmatch(str,"[^%s]")do 
 newList[i] = temp
+i = i+1
 end
-return newList
+return newList, i
 end 
