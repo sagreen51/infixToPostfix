373cb3df1bfa1e94c18829b1d2e57200f763c425
Steven <s.a.green51@gmail.com>
2017-11-25 22:18:08 -0800

    test5

--- prog3_2.lua
+++ prog3_2.lua
@@ -11,10 +11,9 @@ local function pop(index) table.remove(str,index)end
 function InfixToPostfix(str)
 	list[0] = " "
 	for i = 0, strlen(str)-1, 1 do 
-	if str[i] == '+' or str[i] =="-"
+	if str[i] == '+' or str[i] =='-'
 	push(str[i])
 	pop(i)
-	else 
 	end
 	end
 	strcat(str,list)
