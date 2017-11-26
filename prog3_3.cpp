f87ef127517e0e217a1924d6898ddb8cc097ebaf
Steven <s.a.green51@gmail.com>
2017-11-25 23:50:12 -0800

    final submission :(

--- prog3_3.cpp
+++ prog3_3.cpp
@@ -1,4 +1,14 @@
-void main(){
+#include <stdio.h>
+
+#include <lua.hpp>
+
+int main(){
+
+printf("Assignment 3-3, Steven Green, s.a.green51@gmail.com");
+lua_State *state = luaL_newState();
+luaL_openLibs(state);
+luaL_dofile(state,argv[1];
 
 
+return 0;
 }
