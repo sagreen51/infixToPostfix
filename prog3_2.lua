81d96a4cfe02150a1b9882b9aa47c32ca3ce741e
Steven <s.a.green51@gmail.com>
2017-11-25 22:12:33 -0800

    test5

--- prog3_2.lua
+++ prog3_2.lua
@@ -1,25 +1,20 @@
+
 local list  = {}
 temp = {}
 newList = {}
 
 local function push(value)
-	list[0] = ' '
 	table.insert(list,value)
-	strcat(list, " ")
 end
+local function pop(index) table.remove(str,index)end
 
 function InfixToPostfix(str)
-	newList[0] = ' '
-	for temp in string.gmatch(str,"[^%s]+")do 
-
-	if temp == "+" or temp == "-" then
-		push(temp)  
-	else
-		 strcat(newList,temp)
-		 strcat(newList, " ") 
+	list[0] = " "
+	for i = 0, strlen(str)-1, 1 do 
+	if str[i] == '+' or str[i] =="-"
+	push(str[i])
+	pop(i)
+	else 
 	end
 	end
-	total = 19
-	strcat(newList, list)
-	return newList, total
-end 
+end
