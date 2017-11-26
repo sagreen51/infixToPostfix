8e1d5ca68fbd9c84ffd2b31c0e195c4ca9f29f3b
Steven <s.a.green51@gmail.com>
2017-11-25 23:07:23 -0800

    ...

--- prog3_2.lua
+++ prog3_2.lua
@@ -8,13 +8,13 @@ function InfixToPostfix(str)
 --	print("Assignment 3-2, Steven Green, s.a.green51@gmail.com")
 	list[0] = " "
 	for string in string.gmatch(str, "[^%s]+")do 
-	if string == "+" or string == "-"then 
+	if string == '+' or string == '-' then 
 		push(string)
 	else then
-		list[i] = str
+		list[i] = string
 		i = i+1
 	print(list[i])
 	end
 	end
-	return tostring(list)
+	return table.tostring(list)
 end
