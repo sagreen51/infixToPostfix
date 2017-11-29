ef00e01d4b6e5669e86ea152724d82d7a94862e6
Steven <s.a.green51@gmail.com>
2017-11-28 17:14:40 -0800

    stuff

--- prog3_3.cpp
+++ prog3_3.cpp
@@ -3,8 +3,6 @@
 
 #include <lua.hpp>
 
-#include <prog3_3.lua>
-
 int main(int argc, char *argv[]){
 
 printf("Assignment #3-3, Steven Green, s.a.green51@gmail.com");
@@ -12,8 +10,12 @@ lua_State *state = luaL_newstate();
 luaL_openlibs(state);
 luaL_dofile(state,argv[1]);
 
-char str[] = InfixToPostfix(state);
-printf("%c",str);
+lua_getGlobal(state, InfixToPostfix);
+lua_pushstring(state,argv[1]);
+lua_call(state, 1, 1);
+const char (state);
+
+
 return 0;
 }
 
