d115335df6ea314cdb075fecff4828cd73083749
Steven <s.a.green51@gmail.com>
2017-11-30 11:51:36 -0800

    test

--- prog3_2.lua
+++ prog3_2.lua
@@ -10,7 +10,7 @@ function InfixToPostfix(str)
 	i = 1
 	j = 1
 	for string in string.gmatch(str, "[^%s+]")do 
-	if string == '+' or string == '-' then 
+	if string == "+" or string == "-" then 
 		temp[j]= string
 		j = j+1
 	else
