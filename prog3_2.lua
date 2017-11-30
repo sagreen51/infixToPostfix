a7a3713069bcf0cb632dd378c5662050fe51916a
Steven <s.a.green51@gmail.com>
2017-11-30 15:52:29 -0800

    test

--- prog3_2.lua
+++ prog3_2.lua
@@ -4,18 +4,36 @@ temp = {}
 local precendence = {["+"] = 2;["-"] = 2; ["*"] =3;["/"]=3}
 
 function InfixToPostfix(str)
---	print("Assignment 3-2, Steven Green, s.a.green51@gmail.com")
-	list[0] = " "
-	temp[0] = " "
-	for char in string.gmatch(str, "[^%s+]")do 
-	if char== ("%d") or char == ("%a") then table.insert(list,char)
-	else if char ==("+")or char == "-" or char =="*" or char == "/" then
+--	print("Assignment 3-2, Steven Green, s.a.green51@gmail.com")	
+	i = i+1
+
+	for char in string.gmatch(str, "[^%s]")do 
+	if char=="*"or char=="/"or char=="+"or char=="-" then
+		if temp[1] == nil then table.insert(temp,char)
+		else if (char=="+"or char=="-")then
+			while(temp[1]=="*"or temp[1]=="/") do
+				table.insert(list,table.remove(temp,1)) end
+		table.insert(temp,char)
+		else if (temp[1]=="+"or temp[1]=="-")then 
+			table.insert(temp,char)
+		else table.insert(list,char)
+		end
+	else table.insert(list,char) end
+	i = i+1
+
+end
+
+
+
+
+
+
+	else 
 		while (#temp ~= 0  and precendence[char] <= precendence[temp[#temp]])do
 			table.insert(list,temp[#temp])
 			table.remove(temp,#temp)
 		end
 		table.insert(temp,char)
-		end
 	end
 	end
 	while(#temp ~= 0) do 
