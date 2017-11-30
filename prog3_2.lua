87d71046de0c5b27f7852a85f95d006f352a1936
Steven <s.a.green51@gmail.com>
2017-11-30 14:54:41 -0800

    test

--- prog3_2.lua
+++ prog3_2.lua
@@ -5,23 +5,22 @@ local precendence = {["+"] = 2;["-"] = 2; ["*"] =3;["/"]=3}
 
 function InfixToPostfix(str)
 --	print("Assignment 3-2, Steven Green, s.a.green51@gmail.com")
---	list[0] = " "
---	temp[0] = " "
+	list[0] = " "
+	temp[0] = " "
 	for char in string.gmatch(str, "[^%s]")do 
-	if char:match("%d") or char:match("%a") then table.insert(list,char)
-	else then 
-		if char:match("[%+%-%*%/]") then
-	while (temp[#temp-1] ~= 0 and precendence[char] <= precendence[temp[#temp-1]]do
-			table.insert(list,temp[#temp-1])
-			table.remove(temp,#temp-1)
+	if char== ("%d") or char == ("%a") then table.insert(list,char)
+	else if char ==("[%+%-%*%/]") then
+		while (temp[#temp-1] ~= 0 and precendence[char] <= precendence[temp[#temp-1]]do
+			table.insert(list,temp[#temp+1])
+			table.remove(temp,#temp+1)
 		end
 		table.insert(temp,char)
 		end
 	end
 	end
-	while(temp[#temp-1]~=nil) do 
-		table.insert(list,temp[#temp-1])
-		table.remove(temp,#temp-1]
+	while(temp[#temp+1]~=nil) do 
+		table.insert(list,temp[#temp+1])
+		table.remove(temp,#temp+1]
 	end
 	return table.concat(list," ")
 end
