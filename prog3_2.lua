d44a29ac7f2bdbcf32f611bc91914d0eb9f3c31c
Steven <s.a.green51@gmail.com>
2017-11-30 15:33:42 -0800

    test

--- prog3_2.lua
+++ prog3_2.lua
@@ -5,9 +5,9 @@ local precendence = {["+"] = 2;["-"] = 2; ["*"] =3;["/"]=3}
 
 function InfixToPostfix(str)
 --	print("Assignment 3-2, Steven Green, s.a.green51@gmail.com")
-	list[1] = " "
-	temp[1] = " "
-	for char in string.gmatch(str, "[^%s]")do 
+	list[0] = " "
+	temp[0] = " "
+	for char in string.gmatch(str, "[^%s+]")do 
 	if char== ("%d") or char == ("%a") then table.insert(list,char)
 	else if char ==("+")or char == "-" or char =="*" or char == "/" then
 		while (#temp ~= 0  and precendence[char] <= precendence[temp[#temp]])do
