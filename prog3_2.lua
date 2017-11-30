3e183a57ee1d1e04103a8a0b999751f5260b95a3
Steven <s.a.green51@gmail.com>
2017-11-29 18:11:36 -0800

    test

--- prog3_2.lua
+++ prog3_2.lua
@@ -5,7 +5,7 @@ local function push(value)table.insert(temp,value)end
 
 function InfixToPostfix(str)
 --	print("Assignment 3-2, Steven Green, s.a.green51@gmail.com")
---	list[0] = ""
+	list[0] = " "
 	
 	i = 1
 	for string in string.gmatch(str, "[^%s+]")do 
