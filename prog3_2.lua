017e27caabe2ddee5891dc2cd3305c73da801d99
Steven <s.a.green51@gmail.com>
2017-11-30 19:31:46 -0800

    test

--- prog3_2.lua
+++ prog3_2.lua
@@ -1,39 +1,32 @@
-local list  = {}
-local stack = {}
+list 
+stack = {}
 
 local precedence = {["+"] = 2;["-"] = 2; ["*"] =3;["/"]=3}
 
-local function push(value)stack[size()+1]=value end
+local function push(value)table.insert(stack,value) end
 local function pop()
-	local temp = peek()
+	temp = peek()
 	table.remove(stack,size())
 	return temp 
 	end
 local function size()return #stack end
-local function peek()return stack[size()] end
+local function peek()return stack[#stack] end
 
 local function InfixToPostfix(str)
 --	print("Assignment 3-2, Steven Green, s.a.green51@gmail.com")	
 	list[1] = " "
-	stack[1] = " "
-	i = 1
+--	stack[1] = " "
 	for char in string.gmatch(str, "[^%s]")do 
-	if char:match("%d") or char:match("%a") then
-		list[i] = char
-		i = i+1 
+	if char:match("%d") or char:match("%a") then table.insert(list,char)
 	else if char:match("[%+%-%*%/]") then 
 	
 	while(peek()~= nil and precedence[char] <= precedence[peek()]) do
-		list[i] = pop()
-		i = i+1
-		end	
+		table.insert(list,pop())end
+
 		push(char)
 	else push(char) 	
 	end 
 	end
-	while (peek() ~=nil) do
-		list[i]= pop() 
-		i = i+1
-	end 
+	while (peek() ~=nil) do	table.insert(list,pop())end 
 	return table.concat(list," ")
 end 
