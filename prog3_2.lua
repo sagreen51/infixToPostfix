0457196111920b2bf44537d35f2018cc9266f945
Steven <s.a.green51@gmail.com>
2017-11-30 21:54:51 -0800

    test

--- prog3_2.lua
+++ prog3_2.lua
@@ -1,24 +1,24 @@
-list ={}
-stack ={}
+local list ={}
+local stack ={}
 
 local precedence = {["+"] = 2;["-"] = 2; ["*"] =3;["/"]=3}
 
-local function push(value)table.insert(stack,value) end
+local function push(value)table.insert(stack,tostring.value) end
 local function pop()
 	temp = peek()
 	table.remove(stack,size())
 	return temp 
 	end
 local function size()return table.maxn(stack) end
-local function peek()return stack[size()] end
+local function peek()return tostring.stack[size()] end
 
 local function InfixToPostfix(str)
 --	print("Assignment 3-2, Steven Green, s.a.green51@gmail.com")	
     
-	for c in string.gmatch(str, "[^%s]")do 
+	for c in string.gmatch(str, "[^%s+]")do 
 	    if c=="+"or c=="-"or c=="*"or c=="/" then 
-            if size() == 0 then push(c)
-            while(size()>0 and precedence[c] <= precedence[peek()]do 
+            if stack[1] == nil then push(c)
+            while(stack[1] ~= nil and precedence[c] <= precedence[peek()]do 
                  table.insert(list,pop())end
             else push(c)
             end    
@@ -27,7 +27,7 @@ local function InfixToPostfix(str)
 	    end 
 	end
 
-	while (peek() ~= nil) do table.insert(list,pop())end 
+	while (stack[1] ~= nil) do table.insert(list,pop())end 
 	return table.concat(list," ")
 end 
 
