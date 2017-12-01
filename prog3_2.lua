729c72a480b8d09957b8f506df80fdde01781f26
Steven <s.a.green51@gmail.com>
2017-11-30 18:22:18 -0800

    test

--- prog3_2.lua
+++ prog3_2.lua
@@ -3,10 +3,9 @@ stack = {}
 
 local precedance = {["+"] = 2;["-"] = 2; ["*"] =3;["/"]=3}
 
-local function push(value)stack[#stack+1] = value end
+local function push(value) table.insert(stack,value) end
 local function pop()
-	local temp = stack[#stack]
-	stack[#stack] = nil
+	local temp = table.remove(size())
 	return temp 
 	end
 local function size() return #stack end
@@ -14,20 +13,20 @@ local function peek() return stack[size()] end
 
 function InfixToPostfix(str)
 --	print("Assignment 3-2, Steven Green, s.a.green51@gmail.com")	
-	list[1]= ""
-	stack[1] = ""
+	list[1]= " "
+	stack[1] = " "
 	for char in string.gmatch(str, "[^%s]")do 
 	if char=="*"or char=="/"or char=="+"or char=="-" then
 		if size() ==0 then stack[1] = char 
 		else if size() > 0 and precedance[char] <= precedance[peek()]
 			table.insert(list,pop())
 			push(char)
-		else list[#list+1] = char 
+		else  table.insert(list,char) 
 		end
 	else table.insert(list,char)
 	end 
 	end
 	string = table.concat(list," ")
 	print("%s",string)
---	return table.concat(list, " ")
+	return string
 end 
