89c2a9d0bb576093a65a3e4c8f544dfd84b2bfb9
Steven <s.a.green51@gmail.com>
2017-11-30 16:17:29 -0800

    test

--- prog3_2.lua
+++ prog3_2.lua
@@ -1,7 +1,14 @@
 list  = {}
-temp = {}
+stack = {}
 
 local precendence = {["+"] = 2;["-"] = 2; ["*"] =3;["/"]=3}
+
+local function push(value)stack[#stack+1] = value end
+local function pop()
+	local temp = stack[#stack]
+	stack[stack] = nil
+	return temp 
+	end
 
 function InfixToPostfix(str)
 --	print("Assignment 3-2, Steven Green, s.a.green51@gmail.com")	
@@ -9,40 +16,13 @@ function InfixToPostfix(str)
 	temp[1] = " "
 	for char in string.gmatch(str, "[^%s]")do 
 	if char=="*"or char=="/"or char=="+"or char=="-" then
-		if temp[1] == nil then table.insert(temp,char)
-		else if (char=="+"or char=="-")then
-			while(temp[1]=="*"or temp[1]=="/") do
-				table.insert(list,temp[1])
-				table.remove(temp,1)
-			end
-		table.insert(temp,char)
-		else if (temp[1]=="+"or temp[1]=="-")then 
-			table.insert(temp,char)
-		else table.insert(list,char)
+		if stack[1] == nil then stack[1] = char 
+		else if stack[1] ~= nil and precendence[char] <=precendence[stack] then
+			list[#list] = pop
+			push(char)
 		end
 	else table.insert(list,char)
 	end 
 	end
 	return table.concat(list, " ")
-end
-
-
-
-
-
-
-	else 
-		while (#temp ~= 0  and precendence[char] <= precendence[temp[#temp]])do
-			table.insert(list,temp[#temp])
-			table.remove(temp,#temp)
-		end
-		table.insert(temp,char)
-	end
-	end
-	while(#temp ~= 0) do 
-		table.insert(list,temp[#temp])
-		table.remove(temp,#temp]
-	end
---	print("%s",table.concat(list," ")
-	return table.concat(list," ")
-end
+end 
