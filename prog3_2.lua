e286eb32fa983eaa5bffb7b069baaf4794a805ce
Steven <s.a.green51@gmail.com>
2017-11-30 16:22:49 -0800

    test

--- prog3_2.lua
+++ prog3_2.lua
@@ -6,14 +6,14 @@ local precendence = {["+"] = 2;["-"] = 2; ["*"] =3;["/"]=3}
 local function push(value)stack[#stack] = value end
 local function pop()
 	local temp = stack[#stack]
-	stack[stack] = nil
+	stack[#stack] = nil
 	return temp 
 	end
 
 function InfixToPostfix(str)
 --	print("Assignment 3-2, Steven Green, s.a.green51@gmail.com")	
-	list[0]= ""
-	temp[0] = ""
+	list[1]= " "
+	temp[1] = " "
 	for char in string.gmatch(str, "[^%s]")do 
 	if char=="*"or char=="/"or char=="+"or char=="-" then
 		if stack[1] == nil then stack[1] = char 
