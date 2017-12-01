6b35889549631ad011b10200d152303aabc7a25a
Steven <s.a.green51@gmail.com>
2017-11-30 22:22:42 -0800

    test

--- prog3_2.lua
+++ prog3_2.lua
@@ -6,30 +6,27 @@ local function push(value)table.insert(stack,value) end
 local function pop()
 	temp = peek()
 	table.remove(stack,size())
-	return temp 
-	end
-local function size() return table.maxn(stack) end
-local function peek() 
-    if size() == 0 then return 0
-    else return stack[size()] end
+	return temp end
+local function size() return #stack end--table.maxn(stack) end
+local function peek() return stack[size()] end
 
 local function InfixToPostfix(str)
 --	print("Assignment 3-2, Steven Green, s.a.green51@gmail.com")	
 
-local stack = {}
-local list = {}
+stack = {}
+list = {}
     
 	for c in string.gmatch(str, "[^%s]")do 
 	    if c=="+"or c=="-"or c=="*"or c=="/" then 
-            if stack[1] == nil then push(c)
-            else if stack[1] ~= nil then 
+            if stack[1] == nil then push(c) end
+            if stack[1] ~= nil and precedence[c] <= precedence[peek()]then 
                  while(stack[1] ~= nil and precedence[c] <= precedence[peek()]do 
                     table.insert(list,pop())end
+                    push(c)
             else push(c)
             end    
         else 
             table.insert(list,c)
-            print(tostring.list)
 	    end 
 	end
 
