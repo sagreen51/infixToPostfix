bce704e473cc82754eb72aed14960295fc22500f
Steven <s.a.green51@gmail.com>
2017-11-30 21:43:00 -0800

    test

--- prog3_2.lua
+++ prog3_2.lua
@@ -9,7 +9,7 @@ local function pop()
 	table.remove(stack,size())
 	return temp 
 	end
-local function size()return table.maxn(stack) end
+local function size()return table.maxn(stack)-1 end
 local function peek()return stack[size()] end
 
 local function InfixToPostfix(str)
@@ -18,7 +18,7 @@ local function InfixToPostfix(str)
     
 	for c in string.gmatch(str, "[^%s]")do 
 	    if c=="+"or c=="-"or c=="*"or c=="/" then 
-            if size() == nil then push(c)
+            if size() == 0 then push(c)
             while(size()>0 and precedence[c] <= precedence[peek()]do 
                  table.insert(list,pop())end
             else push(c)
