6c481a8adacd8eed9eac477f2c768bbd1380fa15
Steven <s.a.green51@gmail.com>
2017-11-30 21:46:25 -0800

    test

--- prog3_2.lua
+++ prog3_2.lua
@@ -9,12 +9,11 @@ local function pop()
 	table.remove(stack,size())
 	return temp 
 	end
-local function size()return table.maxn(stack)-1 end
+local function size()return table.maxn(stack) end
 local function peek()return stack[size()] end
 
 local function InfixToPostfix(str)
 --	print("Assignment 3-2, Steven Green, s.a.green51@gmail.com")	
-    list[1] = " "
     
 	for c in string.gmatch(str, "[^%s]")do 
 	    if c=="+"or c=="-"or c=="*"or c=="/" then 
