36c70d3eaff7343d6f709ab875146fbfa841055b
Steven <s.a.green51@gmail.com>
2017-11-30 20:01:20 -0800

    test

--- prog3_2.lua
+++ prog3_2.lua
@@ -14,12 +14,24 @@ local function peek()return stack[size()] end
 
 local function InfixToPostfix(str)
 --	print("Assignment 3-2, Steven Green, s.a.green51@gmail.com")	
---	list[1] = " "
---	stack[1] = " "
+
 	for c in string.gmatch(str, "[^%s]")do 
-	if c=="+"
-	end 
+	    if c=="+"or c=="-"or c=="*"or c=="/" then 
+            if size() == 0 then push(c)
+            else if precedence[c] <= precedence[peek()]then table.insert(list,pop())
+            end    
+        else 
+            table.insert(list,c)
+	    end 
 	end
-	while (peek() ~=nil) do	table.insert(list,pop())end 
+
+	while (peek() ~= nil) do table.insert(list,pop())end 
 	return table.concat(list," ")
 end 
+
+
+
+
+
+
+
