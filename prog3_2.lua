5820efa2e2da36a9dec2cae9e5b5baf8086b5638
Steven <s.a.green51@gmail.com>
2017-11-30 19:49:33 -0800

    test

--- prog3_2.lua
+++ prog3_2.lua
@@ -1,4 +1,4 @@
-list ={}
+list ={" "}
 stack = {}
 
 local precedence = {["+"] = 2;["-"] = 2; ["*"] =3;["/"]=3}
@@ -9,21 +9,17 @@ local function pop()
 	table.remove(stack,size())
 	return temp 
 	end
-local function size()return #stack end
-local function peek()return stack[#stack] end
+local function size()return table.maxn(stack) end
+local function peek()return stack[size()] end
 
 local function InfixToPostfix(str)
 --	print("Assignment 3-2, Steven Green, s.a.green51@gmail.com")	
 	list[1] = " "
 --	stack[1] = " "
 	for c in string.gmatch(str, "[^%s]")do 
-	if c:match("%d") or c:match("%a") then table.insert(list,c)
+	if c == "%d" or c == "%a" then table.insert(list,c)
 	else if c:match("[%+%-%*%/]") then 
-	
-	while(peek()~= nil and precedence[c] <= precedence[peek()]) do
-		table.insert(list,pop())end
-
-		push(c)
+		
 	else push(c) 	
 	end 
 	end
