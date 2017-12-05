3abc4c06b765e835fc34bda2b5fcc9a94af7a8ac
Steven <s.a.green51@gmail.com>
2017-12-05 14:28:57 -0800

    test

--- prog3_2.lua
+++ prog3_2.lua
@@ -1,1 +1,35 @@
-local precedence = {["+"] = 2;
+local stack = {}
+
+local precedence = {["+"] = 2;["-"] = 2; ["*"] =3;["/"]=3}
+
+local function push(value)table.insert(stack,value) end
+local function pop() return table.remove(stack,size())
+local function size() return #stack end--table.maxn(stack)end
+local function peek() return stack[size()] end
+
+local function InfixToPostfix(str)
+--	print("Assignment 3-2, Steven Green, s.a.green51@gmail.com")	
+list = {" "}
+
+	for v in string.gmatch(str, "[^%s+]")do 
+	    if v=="+"or v=="-"or v=="*"or v=="/" then 
+            if size() == 0 then push(v)
+            else if size() ~= 0 and precedence[v] <= precedence[peek()]then 
+                 while(size() ~= 0 and precedence[v] <= precedence[peek()]do 
+                    table.insert(list,pop())end
+                 push(v)
+            else table.insert(list,v) end    
+        else table.insert(list,v) end 
+	end
+
+	while (size()~= 0) do table.insert(list,pop())end 
+    string = table.concat(list, " ")
+	return string
+end 
+
+
+
+
+
+
+
