a0da902e42dac34f47e71014e7cce0bcbe3b1929
Steven <s.a.green51@gmail.com>
2017-12-05 11:55:11 -0800

    test

--- prog3_2.lua
+++ prog3_2.lua
@@ -1,40 +1,37 @@
 local stack = {}
 local list = {}
 
-local precedence = {["+"] = 2;["-"] = 2; ["*"] =3;["/"]=3}
-
-local function push(value)table.insert(stack,value) end
-local function pop()
-	temp = peek()
-	table.remove(stack,size())
-	return temp end
-local function size() return #stack end--table.maxn(stack) end
-local function peek() return stack[size()] end
+local precedence = {["+"]=2;["-"]=2; ["*"]=3;["/"]=3}
 
 local function InfixToPostfix(str)
 --	print("Assignment 3-2, Steven Green, s.a.green51@gmail.com")	
     
+    i = 1
+
 	for c in string.gmatch(str, "[^%s]")do 
 	    if c=="+"or c=="-"or c=="*"or c=="/" then 
-            if stack[1] == nil then push(c)
-            if stack[1] ~= nil and precedence[c] <= precedence[peek()]then 
-                 while(stack[1] ~= nil and precedence[c] <= precedence[peek()]do 
-                    table.insert(list,pop())end
-                    push(c)
-            else push(c)
-            end    
+            if stack[1] == nil then stack[1] = c
+            else if (precedence[c] <= precedence[stack[#stack]]) then 
+                while (precedence[c] <= precedence[stack[#stack]]) do 
+                    list[i] = stack[#stack]
+                    table.remove(stack,#stack)
+                    i = i +1
+                    end
+                stack[1] = c
+            else then stack[#stack +1] = c
+            end
         else 
-            table.insert(list,c)
+            list[i] = c
+            i = i+1
 	    end 
 	end
 
-	while (stack[1] ~= nil) do table.insert(list,pop())end 
+	while (stack[1] ~= nil) do 
+           list[i] = stack[#stack]
+           table.remove(stack,#stack)  
+           i = i+1       
+    end 
 	return table.concat(list," ")
 end 
-
-
-
-
-
 
 
