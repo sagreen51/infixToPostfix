d75135a67944ff6cd1989598244a3a8d56474864
Steven <s.a.green51@gmail.com>
2017-12-05 21:19:59 -0800

    test

--- prog3_3.cpp
+++ prog3_3.cpp
@@ -5,20 +5,20 @@
 
 int main(int argc, char *argv[]){
 
-int result;
+const char *result;
 
 printf("Assignment #3-3, Steven Green, s.a.green51@gmail.com\n");
 lua_State *state = luaL_newstate();
 luaL_openlibs(state);
 luaL_dofile(state,argv[1]);
-lua_call(state,0,0);
+lua_pcall(state,0,0,0);
 
 lua_setglobal(state,"InfixToPostfix");
 //lua_setstring(state,argv[1]);
 
-lua_call(state,1,1);
-result = lua_tonumber(state,-1);
-printf("%d",result);
+lua_pcall(state,1,1,0);
+result = lua_tostring(state,-1);
+printf("%s",result);
 lua_close(state);
 return result;
 }
