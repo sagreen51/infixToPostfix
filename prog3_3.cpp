6c9455a4a36ca86e112dfe4b2038f3d246c8faaa
Steven <s.a.green51@gmail.com>
2017-12-05 21:44:26 -0800

    test

--- prog3_3.cpp
+++ prog3_3.cpp
@@ -18,18 +18,17 @@ lua_pcall(state,0,0,0);
 lua_getglobal(state,"InfixToPostfix");
 //lua_setstring(state,argv[1]);
 
-lua_getglobal(state,"InfixToPostfix");
+lua_setglobal(state,"InfixToPostfix");
 lua_pushstring(state,argv[1]);
-lua_gettable(state,-2);
 
 
-//lua_pcall(state,1,1,0);
+lua_pcall(state,1,1,0);
 s = lua_tostring(state,-1);
-//esult = lua_tonumber(state,-1);
+//result = lua_tonumber(state,-1);
 printf("%s\n",s);
 //printf("%d",result);
 lua_close(state);
-return 19;
+return s;
 }
 
 
