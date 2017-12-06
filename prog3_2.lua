367ca256f34cd0e3ea964b5c1f648beb578cea26
Steven <s.a.green51@gmail.com>
2017-12-05 16:00:44 -0800

    test

--- prog3_2.lua
+++ prog3_2.lua
@@ -1,44 +1,37 @@
-local stack = {}
-local list = {}
-
-local precedence = {["+"] = 2;["-"] = 2; ["*"] =3;["/"]=3}
-
-local function push(value) return table.insert(stack,value) end
-local function pop() return table.remove(stack,size()) end
-local function size() return #stack end--table.maxn(stack)end
-local function peek() return stack[size()] end
-
-local function InfixToPostfix(str)
+function InfixToPostfix(str)
 --	print("Assignment 3-2, Steven Green, s.a.green51@gmail.com")	
-    i = 1
+stack = {}
+list = {}
 
-	for v in string.gmatch(str, "[^%s]")do 
+	for v in str.gmatch(str, "[^%s]")do 
 	    if v=="+"or v=="-"or v=="*"or v=="/" then 
-            if size() == 0 then stack[1] = v
-            elseif (size() ~= 0 and precedence[v] <= precedence[peek()])then 
-                 while(size() ~= 0 and precedence[v] <= precedence[peek()])do 
-                        table[i] = pop()
-                        i = i+1
-                 end 
-                    --table.insert(list,pop())end
+            if #stack == 0 then push(v) end
+            elseif #stack ~= 0 and precedence[v] <= precedence[peek()]then 
+                 while(#stack ~= 0 and precedence[v] <= precedence[stack[#stack]])do 
+                    table.insert(list,pop())end
                  push(v)
-            else list[i] = v
-                i = i +1
-            end    
-        elseif v == "%d" or v == "%a" then
-            list[i] = v
-            i = i +1
-        end 
+            end
+            else table.insert(list,v) end
+        end    
+        else table.insert(list,v) end 
 	end
-
-	while (size()~= 0) do
-            list[i] = pop()
-            i = i+1
-    end
-        -- table.insert(list,pop())end 
+if (#stack ~= 0) then 
+	while (#stack ~= 0) do table.insert(list,pop())end 
+end
     string = table.concat(list, " ")
 	return string
 end 
+
+
+local precedence = {["+"] = 2;["-"] = 2; ["*"] =3;["/"]=3}
+
+local function push(value)table.insert(stack,value) end
+local function pop() return table.remove(stack,#stack) end 
+local function peek() return stack[#stack] end
+
+
+print(InfixToPostfix("3 + 5 * 6 / 2 + 1"))
+print("test")
 
 
 
