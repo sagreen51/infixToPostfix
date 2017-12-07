83490466257e85a519167864dfa68edc42022640
Steven <s.a.green51@gmail.com>
2017-12-06 16:25:15 -0800

    test

--- prog3_3.cpp
+++ prog3_3.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <string>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,27 +7,26 @@
 
 int main(int argc, char *argv[]){
 
-char *s;
-
 printf("Assignment #3-3, Steven Green, s.a.green51@gmail.com\n");
 lua_State *state = luaL_newstate();
 luaL_openlibs(state);
-s = argv[1];
+char *s = argv[1];
 luaL_dofile(state,s);
-lua_call(state,0,0);
+lua_pcall(state,0,0,0);
 std::string inputString;
 std::getline(std::cin,inputString);
 
-lua_getglobal(state,"InfixToPostfix");
+
+lua_setglobal(state,"InfixToPostfix");
 
 
 //lua_setglobal(state,"InfixToPostfix");
 lua_pushstring(state,inputString.c_str());
 
-lua_call(state,1,1);
-std::string output = lua_tostring(state,1);
+lua_pcall(state,1,1,0);
+std::string output = lua_tostring(state,-1);
 std::cout<<output<<std::endl;
-std::cout<<lua_tostring(state,2)<<std::endl;
+std::cout<<lua_tostring(state,-1)<<std::endl;
 
 
 lua_close(state);
