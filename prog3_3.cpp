282815f3d6d750aa1cee5c67fb4e69229b663660
Steven <s.a.green51@gmail.com>
2017-12-06 16:16:11 -0800

    test

--- prog3_3.cpp
+++ prog3_3.cpp
@@ -26,7 +26,7 @@ lua_pushstring(state,inputString.c_str());
 
 lua_call(state,1,1);
 std::string output = lua_tostring(state,1);
-std::cout<<output<<endl;
+std::cout<<output<<std::endl;
 std::cout<<lua_tostring(state,2)<<std::endl;
 
 
