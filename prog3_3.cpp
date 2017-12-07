a4815613a0931d6179857c768c1fd7b458ff7c53
Steven <s.a.green51@gmail.com>
2017-12-06 16:09:21 -0800

    test

--- prog3_3.cpp
+++ prog3_3.cpp
@@ -25,7 +25,7 @@ lua_getglobal(state,"InfixToPostfix");
 lua_pushstring(state,inputString.c_str());
 
 lua_call(state,1,1);
-std::cout<<lua_getstring(state,-1)<<std::endl;
+std::cout<<lua_checkstring(state,2)<<std::endl;
 
 
 lua_close(state);
